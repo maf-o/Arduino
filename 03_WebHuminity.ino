629e7cfabe98a40d502c2a7391a81287aa4687dc
ninfan <ninfan@users.noreply.github.com>
2015-07-23 14:45:54 +0000

    Update 03_WebHuminity.ino

--- 03_WebHuminity.ino
+++ 03_WebHuminity.ino
@@ -1,18 +1,9 @@
 /*
   Web Server
- 
- A simple web server that shows the value of the analog input pins.
- using an Arduino Wiznet Ethernet shield. 
- 
+
  Circuit:
  * Ethernet shield attached to pins 10, 11, 12, 13
- * Analog inputs attached to pins A0 through A5 (optional)
- 
- created 18 Dec 2009
- by David A. Mellis
- modified 9 Apr 2012
- by Tom Igoe
- 
+
  Huminity Library von https://github.com/adafruit/DHT-sensor-library
  
  */
