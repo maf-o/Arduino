1502c2d4f1a952aba6b6d688b9e8505c4518871d
ninfan <ninfan@users.noreply.github.com>
2015-08-02 02:05:12 +0000

    Update 04_an_aus.ino

--- 04_an_aus.ino
+++ 04_an_aus.ino
@@ -22,11 +22,11 @@ boolean ausgang = true;
 
 void setup() {
   
-  pinMode(0, INPUT);
-  pinMode(1, INPUT);
   pinMode(2, INPUT);
-  pinMode(6, INPUT);
-  pinMode(3, OUTPUT);
+  pinMode(3, INPUT);
+  pinMode(4, INPUT);
+  pinMode(5, INPUT);
+  pinMode(6, OUTPUT);
   
   
   an = 10;
@@ -67,46 +67,35 @@ void writeArduinoOn7Segment() {
   delay(delaytime);
 } 
 
-/*
-  This method will scroll all the hexa-decimal
- numbers and letters on the display. You will need at least
- four 7-Segment digits. otherwise it won't really look that good.
- */
-void scrollDigits() {
-  for(int i=0;i<13;i++) {
-    lc.setDigit(0,3,i,false);
-    lc.setDigit(0,2,i+1,false);
-    lc.setDigit(0,1,i+2,false);
-    lc.setDigit(0,0,i+3,false);
-    delay(delaytime);
-  }
-  lc.clearDisplay(0);
-  delay(delaytime);
-}
-
 
 
 void loop() { 
   
-  int down_an = digitalRead(0);
-  int up_an = digitalRead(6);
-  int down_aus = digitalRead(2);
-  int up_aus = digitalRead(1);
+  int down_an = digitalRead(2);
+  int up_an = digitalRead(3);
+  int down_aus = digitalRead(4);
+  int up_aus = digitalRead(5);
   
   if (down_an == HIGH) {
     an = an - 10;
-    time = millis() + an;
+    if (an < 0){
+      an = 0;
+    }
+    time = millis() + an* 1000;
   }
   
   if (up_an == HIGH) {
     an = an + 10;
-    time = millis() + an;
+    time = millis() + an* 1000;
   }
   
   
   
   if (down_aus == HIGH) {
     aus = aus - 10;
+    if (aus < 0){
+      aus = 0;
+    }
 
   }
   
@@ -121,11 +110,11 @@ void loop() {
     
     if (ausgang == false){
       
-      time = millis() + aus;
+      time = millis() + aus* 1000;
     
     } else {
       
-      time = millis() + an;
+      time = millis() + an* 1000;
     }
   
   }
@@ -141,15 +130,15 @@ void loop() {
     lc.setDigit(0,3,anzeige %10,false);
     
     anzeige = aus;
-    lc.setDigit(1,0,anzeige %10,false);
+    lc.setDigit(0,4,anzeige %10,false);
     anzeige = anzeige/10;
-    lc.setDigit(1,1,anzeige %10,false);
+    lc.setDigit(0,5,anzeige %10,false);
     anzeige = anzeige/10;
-    lc.setDigit(1,2,anzeige %10,false);
+    lc.setDigit(0,6,anzeige %10,false);
     anzeige = anzeige/10;
-    lc.setDigit(1,3,anzeige %10,false);
+    lc.setDigit(0,7,anzeige %10,false);
     
     delay(delaytime);
-    digitalWrite(3,ausgang);
+    digitalWrite(6,ausgang);
 
 }
